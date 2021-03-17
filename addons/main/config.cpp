6468801d36b0b02df57f688d67730d6f0de132c1
Filip Maciejewski <veteran29@users.noreply.github.com>
2021-03-17 20:41:44 +0100

    Initial commit

---
+++ addons/main/config.cpp
@@ -0,0 +1,16 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            // we're leaving this empty intentionaly to be the one of the first addons executed via XEH (before CBA Settings)
+        };
+        authors[] = {"veteran29"};
+        author = "ArmaForces";
+        VERSION_CONFIG;
+    };
+};
---
+++ addons/main/script_macros.hpp
@@ -0,0 +1,15 @@
+#include "\x\cba\addons\main\script_macros_common.hpp"
+#define DFUNC(module) TRIPLES(ADDON,fnc,module)
+#ifdef DISABLE_COMPILE_CACHE
+    #undef PREP
+    #define PREP(fncName) DFUNC(fncName) = compile preprocessFileLineNumbers QPATHTOF(functions\DOUBLES(fnc,fncName).sqf)
+#else
+    #undef PREP
+    #define PREP(fncName) [QPATHTOF(functions\DOUBLES(fnc,fncName).sqf), QFUNC(fncName)] call CBA_fnc_compileFunction
+#endif
+
+// AFMS macros
+#define IS_MOD_LOADED(modclass)     (isClass (configFile >> "CfgPatches" >> #modclass))
+
+// AFMS debug macros
+#include "\z\afms\addons\main\script_debug.hpp"
